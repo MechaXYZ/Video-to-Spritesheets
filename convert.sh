287f73a1bd5a99755f95386f7be45a233a38bacc
MechaXYZ <93439705+MechaXYZ@users.noreply.github.com>
2024-02-08 19:07:36 -0600

    use -force_key_frames so each segment is exact

--- convert.sh
+++ convert.sh
@@ -3,7 +3,7 @@ out=$(echo "$1" | cut -f 1 -d '.')
 mkdir out/$out
 mkdir out/$out/segs
 echo "splitting $1 into segments of $2 seconds with an fps of $3 and a width of $4"
-ffmpeg -i $1 -reset_timestamps 1 -r $3 -filter:v fps=$3 -filter:v scale=$4:-1 -map 0 -segment_time 00:00:$2 -f segment "out/$out/segs/%d.mp4"
+ffmpeg -i $1 -reset_timestamps 1 -force_key_frames "expr:gte(t,n_forced*$2)" -r $3 -filter:v fps=$3 -filter:v scale=$4:-1 -map 0 -segment_time 00:00:$2 -f segment "out/$out/segs/%d.mp4"
 
 mkdir "out/$out/gifs"
 mkdir "out/$out/frames"
